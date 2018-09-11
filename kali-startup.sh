85d087a647457e8df3088029d41381f429a1c6e7
James Holland <jamoiholland@gmail.com>
2018-09-11 15:54:19 +0100

    Update kali-startup.sh

--- kali-startup.sh
+++ kali-startup.sh
@@ -4,3 +4,4 @@
 sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 sudo chmod 755 msfinstall
 sudo ./msfinstall
+# *** NEED TO ADD EXPECT SCRIPT (OR SIMILAR) TO INITIALISE DATABASE ***
