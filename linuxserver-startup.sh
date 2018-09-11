a70f50cab526cca58e0cf413de9e7f5bc5d062a5
James Holland <jamoiholland@gmail.com>
2018-09-11 16:30:46 +0100

    Update linuxserver-startup.sh

--- linuxserver-startup.sh
+++ linuxserver-startup.sh
@@ -3,9 +3,9 @@
 
 sudo apt-get update
 sudo apt-get install tomcat7 tomcat7-admin default-jre unzip -y
-wget https://archive.apache.org/dist/struts/2.5.12/struts-2.5.12-all.zip
-unzip struts-2.5.12-all.zip
-sudo cp struts-2.5.12/lib/* /usr/share/tomcat7/lib/
+# wget https://archive.apache.org/dist/struts/2.5.12/struts-2.5.12-all.zip
+# unzip struts-2.5.12-all.zip
+# sudo cp struts-2.5.12/lib/* /usr/share/tomcat7/lib/
 wget https://repo1.maven.org/maven2/org/apache/struts/struts2-rest-showcase/2.5.12/struts2-rest-showcase-2.5.12.war
 sudo cp struts2-rest-showcase-2.5.12.war /var/lib/tomcat7/webapps
 sudo service tomcat7 restart
