b2be533e92691371c6b9762abdbe6ef72a97fa33
James Holland <jamoiholland@gmail.com>
2018-09-20 22:09:33 +0100

    Update kali-startup.sh

--- kali-startup.sh
+++ kali-startup.sh
@@ -6,4 +6,7 @@ sudo yum install expect -y
 sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 sudo chmod 755 msfinstall
 sudo ./msfinstall
-# *** NEED TO ADD EXPECT SCRIPT (OR SIMILAR) TO INITIALISE DATABASE ***
+#
+sudo curl https://raw.githubusercontent.com/jamesholland-uk/auto-hack-cloud/master/sploit-init.sh > sploit-init.sh
+sudo chmod 755 sploit-init.sh
+sudo sploit-init.sh
