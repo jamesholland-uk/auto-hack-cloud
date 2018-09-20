138eebc563afb69619d6caefa1c5e3f18f703050
James Holland <jamoiholland@gmail.com>
2018-09-20 21:57:14 +0100

    Update kali-startup.sh

--- kali-startup.sh
+++ kali-startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Initialise the Kali instance
 
+sudo yum install expect -y
+#
 sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 sudo chmod 755 msfinstall
 sudo ./msfinstall
