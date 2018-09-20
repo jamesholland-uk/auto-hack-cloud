be2f3cd7eeeba700311e93ad8609166fd3b09c26
James Holland <jamoiholland@gmail.com>
2018-09-20 17:37:38 +0100

    Update linuxserver-startup.sh

--- linuxserver-startup.sh
+++ linuxserver-startup.sh
@@ -11,7 +11,10 @@ sudo apt-get install tomcat7 tomcat7-admin default-jre unzip -y
 # wget https://repo1.maven.org/maven2/org/apache/struts/struts2-rest-showcase/2.5.12/struts2-rest-showcase-2.5.12.war
 # sudo cp struts2-rest-showcase-2.5.12.war /var/lib/tomcat7/webapps
 #
-wget https://github.com/jamesholland-uk/auto-hack-cloud/blob/master/struts2_2.3.15.1-showcase.war?raw=true
+wget https://raw.githubusercontent.com/jamesholland-uk/auto-hack-cloud/master/struts2_2.3.15.1-showcase.war
 sudo cp struts2_2.3.15.1-showcase.war /var/lib/tomcat7/webapps
+#
+wget https://raw.githubusercontent.com/jamesholland-uk/auto-hack-cloud/master/tomcat-users.xml
+sudo cp tomcat-users.xml /etc/tomcat7
 #
 sudo service tomcat7 restart
