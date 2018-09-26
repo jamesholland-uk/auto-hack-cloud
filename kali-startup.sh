2183b6a82b7c7f0186d70205321ca10016ae2e59
James Holland <jamoiholland@gmail.com>
2018-09-26 18:10:39 +0100

    Update kali-startup.sh

--- kali-startup.sh
+++ kali-startup.sh
@@ -6,7 +6,7 @@ echo "user:Automation123" | sudo chpasswd
 #
 sudo sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config
 echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
-sudo service sshd restart#
+sudo service sshd restart
 sudo yum install expect -y
 #
 sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
@@ -19,3 +19,12 @@ su -c "./metasploit-initialise-db.sh" -s /bin/sh user
 #
 sudo touch /home/user/.bashrc
 sudo echo "/opt/metasploit-framework/bin/msfconsole" >> /home/user/.bashrc
+#
+#
+sudo yum install git openssl-devel pam-devel zlib-devel autoconf automake libtool -y
+sudo git clone https://github.com/shellinabox/shellinabox.git
+cd shellinabox
+sudo autoreconf -i
+sudo ./configure
+sudo make
+sudo ./shellinaboxd -b -q -t
