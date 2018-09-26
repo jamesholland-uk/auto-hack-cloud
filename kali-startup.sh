849823c0b354aad512c778b15401ca0590c5ece8
James Holland <jamoiholland@gmail.com>
2018-09-26 16:39:40 +0100

    Update kali-startup.sh

--- kali-startup.sh
+++ kali-startup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Initialise the Kali instance
 
+sudo adduser --shell /bin/bash -m user
+echo "user:Automation123" | sudo chpasswd
+#
 sudo yum install expect -y
 #
 sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
@@ -9,6 +12,11 @@ sudo ./msfinstall
 #
 sudo curl https://raw.githubusercontent.com/jamesholland-uk/scripts/master/metasploit-initialise-db.sh > metasploit-initialise-db.sh
 sudo chmod 755 metasploit-initialise-db.sh
-su -c "./metasploit-initialise-db.sh" -s /bin/sh jholland
+su -c "./metasploit-initialise-db.sh" -s /bin/sh user
 #
-echo "/opt/metasploit-framework/bin/msfconsole" >> ~/.bashrc
+sudo touch /home/user/.bashrc
+sudo echo "/opt/metasploit-framework/bin/msfconsole" >> /home/user/.bashrc
+#
+sudo sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
+sudo service sshd restart
