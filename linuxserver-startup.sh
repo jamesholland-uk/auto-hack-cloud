fd08cae05ea45de9be4f26924515673ab101dff1
James Holland <jamoiholland@gmail.com>
2018-10-05 10:15:03 +0100

    Update linuxserver-startup.sh

--- linuxserver-startup.sh
+++ linuxserver-startup.sh
@@ -3,6 +3,7 @@
 #
 sudo adduser --shell /bin/bash --disabled-password --gecos "" user
 echo "user:Automation123" | sudo chpasswd
+sudo usermod -aG google-sudoers user
 #
 sudo sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config
 echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
